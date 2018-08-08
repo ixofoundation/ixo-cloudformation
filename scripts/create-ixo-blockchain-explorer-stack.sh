e4f363a8edbc64c394e6b7709c736dcf736e5477
source-code-smith <herman@sourcecodesmith.com>
2018-08-09 00:30:50 +0200

    Blockchain Explorer deployment

---
+++ scripts/create-ixo-blockchain-explorer-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+STACK_SUFFIX="-$1"
+if [ "$#" -ne 1 ]; then
+    STACK_SUFFIX=""
+fi
+
+aws cloudformation create-stack --stack-name Ixo-Blockchain-Explorer-Stack$STACK_SUFFIX --template-body file://templates/IxoBlockchainExplorerStack.yaml  --parameters file://parameters/IxoBlockchainExplorerStack.parameters.json --profile trustlab.cli --region us-east-1
