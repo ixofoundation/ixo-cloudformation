3efbab366af22c321c22b3d2a2be71c69a8ee8c9
source-code-smith <herman@sourcecodesmith.com>
2018-08-08 20:22:48 +0200

    convenience cli script

---
+++ scripts/create-ixo-networking-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+STACK_SUFFIX="-$1"
+if [ "$#" -ne 1 ]; then
+    STACK_SUFFIX=""
+fi
+
+aws cloudformation create-stack --stack-name Ixo-Networking-Stack$STACK_SUFFIX --template-body file://templates/IxoNetworkingStack.yaml  --profile trustlab.cli --region us-east-1
