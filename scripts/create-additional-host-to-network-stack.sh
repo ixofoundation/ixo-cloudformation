5bab937a635e325580262773e0d9d212d624331c
source-code-smith <herman@sourcecodesmith.com>
2018-08-08 20:00:21 +0200

    web instance on own vpc

--- scripts/create-additional-host-to-network-stack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-STACK_SUFFIX="-$1"
-if [ "$#" -ne 1 ]; then
-    STACK_SUFFIX=""
-fi
-
-aws cloudformation create-stack --stack-name Elysian-Additional-Host-To-Network-Stack$STACK_SUFFIX --template-body file://templates/ElysianAdditionalHostToNetworkStack.yaml  --parameters file://parameters/ElysianAdditionalHostToNetworkStack.parameters.json --profile trustlab.cli --region us-east-1
---
+++ scripts/create-ixo-web-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+STACK_SUFFIX="-$1"
+if [ "$#" -ne 1 ]; then
+    STACK_SUFFIX=""
+fi
+
+aws cloudformation create-stack --stack-name Ixo-Web-Stack$STACK_SUFFIX --template-body file://templates/IxoWebStack.yaml  --parameters file://parameters/IxoWebStack.parameters.json --profile trustlab.cli --region us-east-1
