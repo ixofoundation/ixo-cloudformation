f2bbcd041e209dc02de86344730d60343fca4a3a
source-code-smith <herman@sourcecodesmith.com>
2018-09-17 18:14:33 +0200

    incorporated sdgfutures and split TARGET_ENVIRONMENT from TARGET_IMAGE

--- scripts/create-ixo-networking-stack.sh
+++ scripts/create-ixo-networking-stack.sh
@@ -8,4 +8,4 @@ echo "STACK_SUFFIX: $STACK_SUFFIX"
 echo "TARGET_ENVIRONMENT: $TARGET_ENVIRONMENT"
 echo "TARGET_REGION: $TARGET_REGION"
 
-aws cloudformation create-stack --stack-name Ixo-Networking-Stack$STACK_SUFFIX --template-body file://templates/IxoNetworkingStack.yaml  --profile trustlab.cli --region $TARGET_REGION
+# aws cloudformation create-stack --stack-name Ixo-Networking-Stack$STACK_SUFFIX --template-body file://templates/IxoNetworkingStack.yaml  --profile trustlab.cli --region $TARGET_REGION
