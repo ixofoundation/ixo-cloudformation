17866a8b1b68e85330b337233abbb905c9917999
source-code-smith <herman@sourcecodesmith.com>
2018-11-28 14:47:31 +0200

    Consolidated web and world host environments and removed dependency on EIPs

--- scripts/create-ixo-web-world-stack.sh
+++ scripts/create-ixo-web-world-stack.sh
@@ -14,6 +14,6 @@ sed -i '' "s|%%TargetEnvironment%%|$TARGET_ENVIRONMENT|" "$SCRIPTS_DIR/../parame
 sed -i '' "s|%%ImageTag%%|$IMAGE_TAG|" "$SCRIPTS_DIR/../parameters/IxoWebWorldStack.parameters.json"
 sed -i '' "s|%%NetworkStackName%%|$NETWORK_STACK|" "$SCRIPTS_DIR/../parameters/IxoWebWorldStack.parameters.json"
 
-aws cloudformation create-stack --stack-name Ixo-App-Stack$STACK_SUFFIX --template-body file://templates/IxoWebWorldStack.yaml  --parameters file://parameters/IxoWebWorldStack.parameters.json --profile default --region $TARGET_REGION
+aws cloudformation create-stack --stack-name Ixo-Web-World-Stack$STACK_SUFFIX --template-body file://templates/IxoWebWorldStack.yaml  --parameters file://parameters/IxoWebWorldStack.parameters.json --profile default --region $TARGET_REGION
 
 git checkout "$SCRIPTS_DIR/../parameters/IxoWebWorldStack.parameters.json"
